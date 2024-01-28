6bcd33df4caa687028a045a14b0d4a067776c451
Jay Conar <jaysubhashconar@gmail.com>
2024-01-28 22:47:30 +0600

    Data Collection and Pairs Identification complete.

--- jay/data_collection/main.py
+++ jay/data_collection/main.py
@@ -1,10 +1,13 @@
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
 from pick_instruments import instruments
 from store_data import store_data
 from cointegration import cointegrated_pairs
+from charts import plot_charts
+
+import pandas as pd
 import json
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 if __name__ == "__main__":
 
@@ -24,3 +27,11 @@ if __name__ == "__main__":
         price_data = json.load(file)
         if len(price_data) > 0:
             pairs = cointegrated_pairs(price_data)
+
+    # Plotting charts and saving for backtesting
+        if len(price_data) > 0 and pairs is not None:
+            pairs_data = pd.read_csv("co-integrated_pairs.csv")
+            instrument_1 = pairs_data.iloc[0]['Instrument-1']
+            instrument_2 = pairs_data.iloc[0]['Instrument-2']
+            print("Plotting charts...")
+            plot_charts(instrument_1, instrument_2, price_data)
--- jay/data_collection/settings.py
+++ jay/data_collection/settings.py
@@ -3,10 +3,10 @@ from pybit.unified_trading import HTTP
 test = True
 session = HTTP(testnet=test)
 
-limit = 1000
-interval = 120
+limit = 1000  # Number of candles
+interval = 120  # Length of a candle's data in minutes
+window = 60  # Z Score using the average of last n candles; Higher value for less volatility
 
 symbols = session.get_instruments_info(category="linear")
 if symbols["retMsg"] == "OK":
     symbols = symbols["result"]["list"]
-
